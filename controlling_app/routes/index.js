b3c813c7deaa28b8c12d7b0f9e2c0fd37dae65da
amandafyfy <amandafyfy@gmail.com>
2021-10-03 13:05:44 +0800

    Delete unwanted file

--- controlling_app/routes/index.js
+++
@@ -1,5 +0,0 @@
-      /* GET login page. */ 
-      router.get('/login', function(req, res, next) { 
-        res.render('login', { title: 'Login Page', message:
-         req.flash('loginMessage') }); 
-     }); 
